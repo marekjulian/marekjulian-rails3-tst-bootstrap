afd83c8f6d643238f911a81620a36916f6403812
Marek J. Ryniejski <marekjulian@gmail.com>
2012-08-09 12:21:36 -0700

    Add auth and basic user fields

---
+++ db/migrate/20120809184031_add_basic_details_to_users.rb
@@ -0,0 +1,8 @@
+class AddBasicDetailsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :about, :string
+  end
+end
